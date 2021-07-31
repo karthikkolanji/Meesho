f72e50a0418c369a0dd4c97f9b6d62632b49ddcd
karthik kolanji <karthik.kolanji90@gmail.com>
2021-07-31 17:35:22 +0530

    Feat: adds MainActivity.kt to app module

---
+++ app/src/main/java/com/meesho/assignment/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.meesho.assignment.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.meesho.assignment.R
+import com.meesho.assignment.databinding.ActivityMainBinding
+import com.meesho.base.extensions.viewLifecycleScoped
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by viewLifecycleScoped(ActivityMainBinding::inflate)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.toolbar
+            .setupWithNavController(navController)
+    }
+}
