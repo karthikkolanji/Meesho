c376e158e028d8a461391034dd63336fe0d2d186
karthik kolanji <karthik.kolanji90@gmail.com>
2021-07-31 17:45:49 +0530

    Refactor : optimises imports

--- app/src/main/java/com/meesho/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/meesho/assignment/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.meesho.assignment.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.meesho.assignment.R
