0a4606e62e4c3af36219554bf95357fffe676b96
karthik kolanji <karthik.kolanji90@gmail.com>
2021-07-31 16:41:26 +0530

    Feat : adds State.kt to base module
    - this helps in displaying current states like Loading,Error,Success to UI(Activity,Fragment) while getting data from data source (api call/local db call)

---
+++ base/src/main/java/com/meesho/base/utils/State.kt
@@ -0,0 +1,7 @@
+package com.meesho.base.utils
+
+sealed class State<out T> {
+    object LoadingState : State<Nothing>()
+    data class Success<T>(var data: T) : State<T>()
+    data class ErrorState(var exception: Throwable) : State<Nothing>()
+}
