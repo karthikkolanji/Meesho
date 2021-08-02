fcaff0f5f6393f158c10ec39e15f7b304b228fb7
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-02 20:32:50 +0530

    Refactor: makes viewmodel to use usecase for background task scheduling

--- jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatViewModel.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatViewModel.kt
@@ -2,17 +2,17 @@ package com.meesho.jakewarton.ui
 
 import androidx.lifecycle.ViewModel
 import com.meesho.base.di.DispatcherProvider
-import com.meesho.jakewarton.domain.ParseScanResult
+import com.meesho.jakewarton.data.entity.BookSeat
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class BookSeatViewModel @Inject constructor(
-    private val parseScanResult: ParseScanResult,
+    private val bookSeat: BookSeat,
     private val dispatcherProvider: DispatcherProvider
 ) : ViewModel() {
 
     suspend fun parseScanResult(scanResult: String) {
-        parseScanResult.parse(scanResult)
+        bookSeat.bookSeat(scanResult)
     }
 }
