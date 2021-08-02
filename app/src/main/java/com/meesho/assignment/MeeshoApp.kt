cd0a496550bf69779ae78c61b1920389e5dd03d3
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-02 20:28:51 +0530

    Feat: removes default configuration of workmanager
    - by default app configures work manager at start, this causes un-necessary load during startup time.
    
    - removing default configuration and adding custom will help in on-demand use of workmanager

--- app/src/main/java/com/meesho/assignment/MeeshoApp.kt
+++ app/src/main/java/com/meesho/assignment/MeeshoApp.kt
@@ -1,18 +1,29 @@
 package com.meesho.assignment
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class MeeshoApp:Application() {
+class MeeshoApp : Application(), Configuration.Provider {
 
     @Inject
     lateinit var appLifeCycleObserver: AppLifeCycleObserver
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         ProcessLifecycleOwner.get().lifecycle.addObserver(appLifeCycleObserver)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+      return  Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
     }
 }
