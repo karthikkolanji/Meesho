3860ff88069f33d4612e6c3a4ad731e81f25efda
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-03 18:03:11 +0530

    Feat: adds functionality observe elapsed time

--- jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatFragment.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatFragment.kt
@@ -10,7 +10,9 @@ import androidx.lifecycle.lifecycleScope
 import com.google.zxing.integration.android.IntentIntegrator
 import com.meesho.base.extensions.shortToast
 import com.meesho.base.extensions.viewLifecycleScoped
+import com.meesho.base.utils.State
 import com.meesho.jakewarton.R
+import com.meesho.jakewarton.data.entity.QRScanResult
 import com.meesho.jakewarton.databinding.FragmentBookSeatBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -25,10 +27,44 @@ class BookSeatFragment : Fragment(R.layout.fragment_book_seat) {
         super.onViewCreated(view, savedInstanceState)
         binding.btBookSeat.setOnClickListener {
             lifecycleScope.launch {
-                viewModel.parseScanResult("\"{\\\"location_id\\\":\\\"ButterKnifeLib-1234\\\",\\\"location_details\\\":\\\"ButterKnife Lib, 80 Feet Rd, Koramangala 1A Block, Bangalore\\\",\\\"price_per_min\\\":5.50}\"")
+                viewModel.bookSeat("\"{\\\"location_id\\\":\\\"ButterKnifeLib-1234\\\",\\\"location_details\\\":\\\"ButterKnife Lib, 80 Feet Rd, Koramangala 1A Block, Bangalore\\\",\\\"price_per_min\\\":5.50}\"")
+//               delay(2000)
+//                observerElapsedTime()
+                //viewModel.startTimer()
             }
             //IntentIntegrator.forSupportFragment(this).initiateScan()
         }
+        observerElapsedTime()
+
+    }
+
+    private fun observerElapsedTime() {
+
+        lifecycleScope.launch {
+            viewModel.getElapsedTime().observe(viewLifecycleOwner, {
+
+                when (it) {
+                    is State.LoadingState -> {
+                       // nothing
+                    }
+                    is State.Success<*> -> {
+                       it.data?.let {result->
+                           val time=result as QRScanResult
+                           binding.tvLocationDetails.text = "h:${time.hour} m:${time.minute} s:${time.seconds}"
+                        }
+
+
+                    }
+                    is State.ErrorState -> {
+                        shortToast(it.exception.message)
+                    }
+                }
+                it?.let {
+
+                }
+            })
+        }
+
     }
 
 
@@ -41,8 +77,8 @@ class BookSeatFragment : Fragment(R.layout.fragment_book_seat) {
                 shortToast(R.string.qr_read_error)
             } else {
                 lifecycleScope.launch {
-                    viewModel.parseScanResult(result.contents)
-                    viewModel.parseScanResult("\"{\\\"location_id\\\":\\\"ButterKnifeLib-1234\\\",\\\"location_details\\\":\\\"ButterKnife Lib, 80 Feet Rd, Koramangala 1A Block, Bangalore\\\",\\\"price_per_min\\\":5.50}\"")
+                    // viewModel.parseScanResult(result.contents)
+                    // viewModel.parseScanResult("\"{\\\"location_id\\\":\\\"ButterKnifeLib-1234\\\",\\\"location_details\\\":\\\"ButterKnife Lib, 80 Feet Rd, Koramangala 1A Block, Bangalore\\\",\\\"price_per_min\\\":5.50}\"")
                 }
                 Log.d("onActivityResult", "${result.contents}")
                 shortToast(result.contents)
--- jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatViewModel.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/ui/BookSeatViewModel.kt
@@ -1,18 +1,34 @@
 package com.meesho.jakewarton.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import com.meesho.base.di.DispatcherProvider
+import com.meesho.base.utils.State
 import com.meesho.jakewarton.data.entity.BookSeat
+import com.meesho.jakewarton.domain.GetElapsedTime
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 @HiltViewModel
 class BookSeatViewModel @Inject constructor(
     private val bookSeat: BookSeat,
-    private val dispatcherProvider: DispatcherProvider
+    private val dispatcherProvider: DispatcherProvider,
+    private val getElapsedTime: GetElapsedTime
 ) : ViewModel() {
 
-    suspend fun parseScanResult(scanResult: String) {
+    suspend fun bookSeat(scanResult: String) {
         bookSeat.bookSeat(scanResult)
+    }
+    suspend fun getElapsedTime()= liveData {
+        emit(State.LoadingState)
+        try {
+            getElapsedTime.get().collect {
+                emit(State.Success(it))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(e)
+        }
     }
 }
