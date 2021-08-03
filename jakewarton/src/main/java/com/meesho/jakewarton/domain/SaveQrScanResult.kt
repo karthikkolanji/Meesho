a4e57487a294ecdc4f398bd567dc25c75871c144
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-03 18:11:08 +0530

    Refactor: adds separate usecase for starting timer

--- jakewarton/src/main/java/com/meesho/jakewarton/domain/SaveQrScanResult.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/domain/SaveQrScanResult.kt
@@ -1,9 +1,6 @@
 package com.meesho.jakewarton.domain
 
-import android.util.Log
 import com.meesho.jakewarton.data.db.Repository
-import com.meesho.jakewarton.utils.Utils
-import kotlinx.coroutines.delay
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,27 +8,11 @@ import javax.inject.Singleton
 class SaveQrScanResult @Inject constructor(
     private val repository: Repository,
     private val parseScanResult: ParseScanResult,
-    private val utils: Utils
+    private val startTimer: StartTimer
 ) {
 
     suspend fun saveTime(scanResult: String) {
         repository.saveQrScanResult(parseScanResult.parse(scanResult))
-        startTimer()
-    }
-
-    private suspend fun startTimer() {
-
-        val startTime = System.currentTimeMillis()
-
-        while (true) {
-            val elapsedTime = utils.millisecondToStandard(System.currentTimeMillis() - startTime)
-            repository.updateTime(elapsedTime)
-            Log.d("updateTime", "$elapsedTime ")
-            delay(INTERVAL)
-        }
-    }
-
-    companion object {
-        const val INTERVAL = 1L
+        startTimer.start()
     }
 }
--- jakewarton/src/main/java/com/meesho/jakewarton/domain/StartTimer.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/domain/StartTimer.kt
@@ -1,19 +1,30 @@
 package com.meesho.jakewarton.domain
 
 import android.util.Log
+import com.meesho.jakewarton.data.db.Repository
+import com.meesho.jakewarton.data.entity.Timer
 import com.meesho.jakewarton.utils.Utils
 import kotlinx.coroutines.delay
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class StartTimer @Inject constructor(private val utils: Utils) {
+@Singleton
+class StartTimer @Inject constructor(private val utils: Utils,
+                                     private val repository: Repository,
+) {
 
-    suspend fun start() {
+    suspend fun start(): Timer {
         val startTime = System.currentTimeMillis()
 
-            while (true) {
-                val result=utils.millisecondToStandard(System.currentTimeMillis() - startTime)
-                Log.d("StartTimer", "$result ")
-                delay(1000)
+        while (true) {
+            val elapsedTime = utils.millisecondToStandard(System.currentTimeMillis() - startTime)
+            repository.updateTime(elapsedTime)
+            Log.d("updateTime", "$elapsedTime ")
+            delay(INTERVAL)
         }
+    }
+
+    companion object {
+        const val INTERVAL = 1000L
     }
 }
