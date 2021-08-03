2de0468c1e37042e7532e7e0271dc7b92955998c
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-03 23:32:07 +0530

    Refactor : adds method to cancel timer

--- jakewarton/src/main/java/com/meesho/jakewarton/domain/StartTimer.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/domain/StartTimer.kt
@@ -1,26 +1,38 @@
 package com.meesho.jakewarton.domain
 
-import android.util.Log
 import com.meesho.jakewarton.data.db.Repository
 import com.meesho.jakewarton.utils.Utils
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class StartTimer @Inject constructor(private val utils: Utils,
-                                     private val repository: Repository,
+class StartTimer @Inject constructor(
+    private val utils: Utils,
+    private val repository: Repository,
 ) {
 
+    private val scope =
+        MainScope()
+    private var job: Job? = null
     suspend fun start() {
         val startTime = System.currentTimeMillis()
-
-        while (true) {
-            val elapsedTime = utils.millisecondToStandard(System.currentTimeMillis() - startTime)
-            repository.updateTime(elapsedTime)
-            Log.d("updateTime", "$elapsedTime ")
-            delay(INTERVAL)
+        job = scope.launch {
+            while (true) {
+                val elapsedTime =
+                    utils.millisecondToStandard(System.currentTimeMillis() - startTime)
+                repository.updateTime(elapsedTime)
+                delay(INTERVAL)
+            }
         }
+    }
+
+    fun stopTimer() {
+        job?.cancel()
+        job = null
     }
 
     companion object {
