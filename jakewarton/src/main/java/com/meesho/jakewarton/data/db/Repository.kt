2822517ce916ed34b221e1fca1b63ac9fe092fb8
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-03 18:01:05 +0530

    Feat: adds methods to save & update elapsed time

---
+++ jakewarton/src/main/java/com/meesho/jakewarton/data/db/Repository.kt
@@ -0,0 +1,39 @@
+package com.meesho.jakewarton.data.db
+
+import com.meesho.base.di.DispatcherProvider
+import com.meesho.jakewarton.data.entity.QRScanResult
+import com.meesho.jakewarton.data.entity.Timer
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Repository @Inject constructor(
+    private val dao: BookSeatDao,
+    private val dispatcherProvider: DispatcherProvider
+) {
+
+    suspend fun saveQrScanResult(scanResult: QRScanResult) {
+        dao.insert(scanResult)
+    }
+
+    suspend fun updateTime(timer: Timer) {
+        val scanResult = withContext(dispatcherProvider.io()){
+            dao.getQrResult()
+        }
+        val locationId = scanResult.location_id
+        withContext(dispatcherProvider.io()){
+            dao.updateTime(timer.hour, timer.minute, timer.seconds, locationId)
+        }
+    }
+
+    suspend fun getElapsedTime(): Flow<QRScanResult?> {
+        return dao.getElapsedTime()
+            .flowOn(dispatcherProvider.io())
+            .distinctUntilChanged()
+    }
+
+}
