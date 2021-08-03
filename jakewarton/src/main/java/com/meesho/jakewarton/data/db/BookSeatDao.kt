73e926d8354ff94d4545e2f6f7e6775461ba421f
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-03 18:00:41 +0530

    Feat: adds methods to save & update elapsed time

--- jakewarton/src/main/java/com/meesho/jakewarton/data/db/BookSeatDao.kt
+++ jakewarton/src/main/java/com/meesho/jakewarton/data/db/BookSeatDao.kt
@@ -2,11 +2,22 @@ package com.meesho.jakewarton.data.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.meesho.jakewarton.data.entity.QRScanResult
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BookSeatDao {
     @Insert
     suspend fun insert(qrScanResult: QRScanResult)
+
+    @Query("SELECT * FROM QRScanResult")
+    suspend fun getQrResult(): QRScanResult
+
+    @Query("UPDATE QRScanResult SET hour=:hour,minute=:minute,seconds=:seconds WHERE location_id = :id")
+    suspend fun updateTime(hour: Long, minute: Long, seconds: Long, id: String)
+
+    @Query("SELECT * FROM QRScanResult")
+    fun getElapsedTime(): Flow<QRScanResult?>
 
 }
