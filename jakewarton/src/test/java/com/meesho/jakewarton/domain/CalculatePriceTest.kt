e84d2a8bca1f90e874984e14af2ee8eb1ae32f0a
karthik kolanji <karthik.kolanji90@gmail.com>
2021-08-06 02:04:28 +0530

    Feat: adds test for CalculatePrice.kt

---
+++ jakewarton/src/test/java/com/meesho/jakewarton/domain/CalculatePriceTest.kt
@@ -0,0 +1,102 @@
+package com.meesho.jakewarton.domain
+
+import MockTestUtil
+import com.google.common.truth.Truth
+import com.meesho.jakewarton.data.db.Repository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class CalculatePriceTest {
+    @MockK
+    private lateinit var repository: Repository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+
+    @Test
+    fun `test calculate price and adds to db`() = runBlocking {
+
+        val expectedDuration = 2 // 2 minutes
+        val expectedAmount = 11
+        val startTime = System.currentTimeMillis()
+        val endTime = startTime + 120000 // adding 2 minutes
+        val expectedSession = MockTestUtil.expectedSession().copy(
+            start_time = startTime,
+            end_time = endTime
+        )
+
+        // When
+        coEvery { repository.getSession() }
+            .returns(expectedSession)
+
+        val session = repository.getSession()
+
+        val duration = (session.end_time - session.start_time) / 60000
+        Truth.assertThat(duration).isEqualTo(expectedDuration)
+
+        val amount = session.price_per_min * duration
+        Truth.assertThat(amount).isEqualTo(expectedAmount)
+
+        coVerify(exactly = 1) { repository.getSession() }
+    }
+
+
+    @Test
+    fun `test should fail on incorrect duration`() = runBlocking {
+
+        val expectedDuration = 3 // 3 minutes
+        val startTime = System.currentTimeMillis()
+        val endTime = startTime + 120000 // making session of 2 minutes
+        val expectedSession = MockTestUtil.expectedSession().copy(
+            start_time = startTime,
+            end_time = endTime
+        )
+
+        // When
+        coEvery { repository.getSession() }
+            .returns(expectedSession)
+
+        val session = repository.getSession()
+
+        val duration = (session.end_time - session.start_time) / 60000
+        Truth.assertThat(duration).isNotEqualTo(expectedDuration)
+
+        coVerify(exactly = 1) { repository.getSession() }
+    }
+
+    @Test
+    fun `test should fail on incorrect amount`() = runBlocking {
+
+        val expectedDuration = 3 // 2 minutes
+        val expectedAmount = 15
+        val startTime = System.currentTimeMillis()
+        val endTime = startTime + 120000 // making session of 2 minutes
+        val expectedSession = MockTestUtil.expectedSession().copy(
+            start_time = startTime,
+            end_time = endTime
+        )
+
+        // When
+        coEvery { repository.getSession() }
+            .returns(expectedSession)
+
+        val session = repository.getSession()
+
+        val duration = (session.end_time - session.start_time) / 60000
+        Truth.assertThat(duration).isNotEqualTo(expectedDuration)
+
+        val amount = session.price_per_min * duration
+        Truth.assertThat(amount).isNotEqualTo(expectedAmount)
+
+        coVerify(exactly = 1) { repository.getSession() }
+    }
+
+}
